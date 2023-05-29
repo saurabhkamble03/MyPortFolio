fabd998e922315cfca668c7f8472bf8b4691951b
saurabhkamble03 <94774518+saurabhkamble03@users.noreply.github.com>
2023-05-29 16:50:06 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
             </button>
         </div>
         <div className='links'>
-            <Link to="/">Home</Link>
+            <Link to="/">Homee</Link>
             <Link to="/projects">Projects</Link>
             <Link to="/education">Education</Link>
             <Link to="/experience">Experience</Link>
